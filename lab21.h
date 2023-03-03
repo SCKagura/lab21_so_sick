346eaa2230bfc0faf5e5bc6a6c93a135be109d85
SCKboss <=>
2023-03-03 11:41:46 +0700

    finish

--- lab21.h
+++ lab21.h
@@ -64,10 +64,41 @@ void Unit::newTurn(){
 }
 
 
+bool Unit::isDead(){
+	if(hp<=0)return true;
+	else return false;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+void Unit::guard(){
+	guard_on = true;
+}
+
+int Unit::beAttacked(int oppatk){
+	if(guard_on == false){
+		hp = hp-(oppatk-def);
+		return oppatk-def;
+	}else{
+		hp = hp-((oppatk-def)/3);
+		return (oppatk-def)/3;
+	}
+}
+
+int Unit::attack(Unit &a){
+	return a.beAttacked(atk);
+}
+
+//add heal
+int Unit::heal(){
+	int he=rand()%21+10;
+	if(hp + he >=hpmax){
+		he= hpmax-hp;
+		hp=hpmax;
+	}else{
+		hp = hp + he;
+	}
+	return he;
+}
+
 
 
 
